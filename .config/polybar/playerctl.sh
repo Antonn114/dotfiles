0ab990369c05c384907a1b35873d1097157f367b
Anton <tpyhr2022@gmail.com>
2023-12-05 21:34:31 +0700

    december 5th 2023

--- .config/polybar/playerctl.sh
+++ .config/polybar/playerctl.sh
@@ -3,11 +3,16 @@
 playerctlstatus=$(playerctl status 2> /dev/null)
 playerctlartist=$(playerctl metadata artist)
 playerctltitle=$(playerctl metadata title)
+playerctl_echo="${playerctlartist} - ${playerctltitle}"
+if (( ${#playerctl_echo} > 50 )); then
+  playerctl_echo=$(echo $playerctl_echo | cut -c 1-50)
+  playerctl_echo+="..."
+fi
 if [[ $playerctlstatus ==  "" ]]; then
     echo ""
 elif [[ $playerctlstatus =~ "Playing" ]]; then
-  echo "%{A1:playerctl previous:}<%{A}  %{A1:playerctl pause:}⏸️  $playerctlartist - $playerctltitle%{A}  %{A1:playerctl next:}>%{A}"
+  echo "%{A1:playerctl previous:}<%{A}  %{A1:playerctl pause:}⏸️  $playerctl_echo%{A}  %{A1:playerctl next:}>%{A}"
 else
-  echo "%{A1:playerctl previous:}<%{A}  %{A1:playerctl  play:}▶️  $playerctlartist - $playerctltitle%{A}  %{A1: playerctl next:}>%{A}"
+  echo "%{A1:playerctl previous:}<%{A}  %{A1:playerctl  play:}▶️  $playerctl_echo%{A}  %{A1: playerctl next:}>%{A}"
 fi
 
